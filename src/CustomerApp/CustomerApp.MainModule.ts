45f2903a979fcfdaf30478c2999efa91baaa8f38
AmritaDutta <amydutta97@gmail.com>
2020-10-29 10:42:05 +0530

    11 files added, 4 deleted

--- src/CustomerApp/CustomerApp.MainModule.ts
+++ src/CustomerApp/CustomerApp.MainModule.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { CustomerComponent } from './CustomerApp.component';
+
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ CustomerComponent ],
-  bootstrap:    [ CustomerComponent ]
+  bootstrap:    [ MasterPageComponent ]
 })
-export class CustomerModule { }
+export class MainModule { }
