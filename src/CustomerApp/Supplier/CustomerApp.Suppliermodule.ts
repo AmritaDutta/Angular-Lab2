0c4a0486f62efd1f060e723aba76248bcc8a994c
AmritaDutta <amydutta97@gmail.com>
2020-10-30 11:05:21 +0530

    2 files modified

--- src/CustomerApp/Supplier/CustomerApp.Suppliermodule.ts
+++ src/CustomerApp/Supplier/CustomerApp.Suppliermodule.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { SupplierComponent } from "../../CustomerApp/Supplier/CustomerApp.Suppliercomponent";
 import { SupplierRoute } from "../../CustomerApp/Router/CustomerApp.SupplierRouter";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   imports: [RouterModule.forChild(SupplierRoute), CommonModule, FormsModule],
