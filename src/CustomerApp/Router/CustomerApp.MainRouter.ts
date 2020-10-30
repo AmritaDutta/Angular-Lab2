e0aa51b2fa1f53c0b90bf0deed59b7a72946fa7d
AmritaDutta <amydutta97@gmail.com>
2020-10-30 11:03:33 +0530

    2 files modified

--- src/CustomerApp/Router/CustomerApp.MainRouter.ts
+++ src/CustomerApp/Router/CustomerApp.MainRouter.ts
@@ -2,9 +2,13 @@ import { HomeComponent } from "../../CustomerApp/Home/CustomerApp.Homecomponent"
 
 export const MainRoute = [
   { path: "Home", component: HomeComponent },
-  { path: "Customer/Add", 
-  loadChildren : './Customer/CustomerApp.Customermodule#CustomerModule' },
-  { path: "Supplier/Add", 
-  loadChildren : '../CustomerApp.Suppliermodule#S' },
+  {
+    path: "Customer/Add",
+    loadChildren: "../Customer/CustomerApp.Customermodule#CustomerModule"
+  },
+  {
+    path: "Supplier/Add",
+    loadChildren: "../Supplier/CustomerApp.Suppliermodule#SupplierModule"
+  },
   { path: "", component: HomeComponent }
 ];
--- src/CustomerApp/Supplier/CustomerApp.Suppliermodule.ts
+++ src/CustomerApp/Supplier/CustomerApp.Suppliermodule.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { SupplierComponent } from "../../CustomerApp/Supplier/CustomerApp.Suppliercomponent";
+import { SupplierRoute } from "../../CustomerApp/Router/CustomerApp.SupplierRouter";
 
 @NgModule({
   imports: [RouterModule.forChild(SupplierRoute), CommonModule, FormsModule],
