35e19c515203796404a6753632b40d4ed088003c
AmritaDutta <amydutta97@gmail.com>
2020-10-30 10:55:13 +0530

    1 file modified

--- src/CustomerApp/Router/CustomerApp.MainRouter.ts
+++ src/CustomerApp/Router/CustomerApp.MainRouter.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from "../../CustomerApp/Home/CustomerApp.Homecomponent"
 export const MainRoute = [
   { path: "Home", component: HomeComponent },
   { path: "Customer/Add", 
-  loadChildren : '../CustomerApp.Customermodule#CustomerModule' },
+  loadChildren : './Customer/CustomerApp.Customermodule#CustomerModule' },
   { path: "Supplier/Add", 
-  loadChildren : '../CustomerApp.Suppliermodule#SupplierM' },
+  loadChildren : '../CustomerApp.Suppliermodule#S' },
   { path: "", component: HomeComponent }
 ];
