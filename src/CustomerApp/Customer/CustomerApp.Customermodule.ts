46fdad894a547138cb0380e803c32405d76122ce
AmritaDutta <amydutta97@gmail.com>
2020-11-02 22:11:06 +0530

    1 file modified

--- src/CustomerApp/Customer/CustomerApp.Customermodule.ts
+++ src/CustomerApp/Customer/CustomerApp.Customermodule.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { CustomerComponent } from "../../CustomerApp/Customer/CustomerApp.Customercomponent";
 import { CustomerRoute } from "../../CustomerApp/Router/CustomerApp.CustomerRouter";
 
 @NgModule({
-  imports: [RouterModule.forChild(CustomerRoute), CommonModule, FormsModule],
+  imports: [
+    RouterModule.forChild(CustomerRoute),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [CustomerComponent],
   bootstrap: [CustomerComponent]
 })
