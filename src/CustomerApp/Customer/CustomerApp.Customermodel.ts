2d697313931435d548735e76039dec2844ff9e2e
AmritaDutta <amydutta97@gmail.com>
2020-11-02 21:49:04 +0530

    1 file modified

--- src/CustomerApp/Customer/CustomerApp.Customermodel.ts
+++ src/CustomerApp/Customer/CustomerApp.Customermodel.ts
@@ -1,6 +1,32 @@
-export class Customer
-{
-  CustomerCode:String="";
-  CustomerName:String="";
-  CustomerAmount:Number=0;
+import {
+  NgForm,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+
+export class Customer {
+  CustomerCode: String = "";
+  CustomerName: String = "";
+  CustomerAmount: Number = 0;
+  FormCustomerGroup: FormGroup = null;
+  constructor() {
+    var _builder = new FormBuilder();
+    this.FormCustomerGroup = _builder.group({});
+
+    this.FormCustomerGroup.addControl(
+      "FormNameControl",
+      new FormControl("", Validators.required)
+    );
+
+    var validationcollection = [];
+    validationcollection.push("", Validators.required);
+    validationcollection.push(Validators.pattern("^[0,9]{4,4}$"));
+
+    this.FormCustomerGroup.addControl(
+      "FormCodeControl",
+      new FormControl("", Validators.compose(validationcollection))
+    );
+  }
 }
